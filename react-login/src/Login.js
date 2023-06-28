c5f72ea3110acbb3535bf959deecbc1cb6658548
PONDALONG <alongkorn.mo1999@gmail.com>
2023-06-28 19:28:46 +0700

    edit login page

--- react-login/src/Login.js
+++ react-login/src/Login.js
@@ -77,7 +77,7 @@ export default function SignInSide() {
           md={7}
           sx={{
             backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
+              "url(https://images.unsplash.com/photo-1680459575585-390ed5cfcae0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
